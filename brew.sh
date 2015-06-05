d1e33cd5f4935ddd2d07068b8e90ec7ca1058650
Dang Van Thanh <dangvanthanh@dangthanh.org>
2015-06-05 23:49:55 +0700

    Add new brew.sh

--- brew.sh
+++ brew.sh
@@ -28,7 +28,7 @@ brew install gnu-sed --default-names
 # running `chsh`.
 brew install bash
 brew tap homebrew/versions
-brew install bash-completion2
+brew install homebrew/versions/bash-completion2
 
 # Install `wget` with IRI support.
 brew install wget --with-iri
@@ -40,7 +40,7 @@ brew install ringojs
 brew install narwhal
 
 # Install more recent versions of some OS X tools.
-brew install vim --override-system-vi
+brew install macvim --override-system-vim
 brew install homebrew/dupes/grep
 brew install homebrew/php/php55
 
