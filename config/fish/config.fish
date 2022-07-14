dc305570be615b7022d8d7139d0ab860dd226af3
Dang Van Thanh <dangvanthanh@dangthanh.org>
2022-07-15 01:01:36 +0700

    Add bun

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -9,3 +9,7 @@ export PATH="$HOME/.cargo/bin:$PATH"
 
 # The next line updates PATH for Netlify's Git Credential Helper.
 test -f '/Users/dangvanthanh/Library/Preferences/netlify/helper/path.fish.inc' && source '/Users/dangvanthanh/Library/Preferences/netlify/helper/path.fish.inc'
+# bun
+set -Ux BUN_INSTALL "/Users/dangvanthanh/.bun"
+fish_add_path "/Users/dangvanthanh/.bun/bin"
+
