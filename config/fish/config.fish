db61d79a9ae3a92931ca358e100299a4f8927335
Dang Van Thanh <dangvanthanh@dangthanh.org>
2022-07-21 23:39:55 +0700

    Update path for prince

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -7,6 +7,9 @@ source /opt/homebrew/opt/asdf/libexec/asdf.fish
 # Rust
 export PATH="$HOME/.cargo/bin:$PATH"
 
+# Prince
+export PATH="$HOME/Downloads/prince-14.3-macos/lib/prince/bin:$PATH"
+
 # The next line updates PATH for Netlify's Git Credential Helper.
 test -f '/Users/dangvanthanh/Library/Preferences/netlify/helper/path.fish.inc' && source '/Users/dangvanthanh/Library/Preferences/netlify/helper/path.fish.inc'
 # bun
