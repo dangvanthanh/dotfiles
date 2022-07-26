39cf564f8317b5ac8ab8f8a4dad827523048e214
Dang Van Thanh <dangvanthanh@dangthanh.org>
2022-07-26 23:00:02 +0700

    Change path for /Users/dangvanthanh

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -7,12 +7,9 @@ source /opt/homebrew/opt/asdf/libexec/asdf.fish
 # Rust
 export PATH="$HOME/.cargo/bin:$PATH"
 
-# Prince
-export PATH="$HOME/Downloads/prince-14.3-macos/lib/prince/bin:$PATH"
+# Bun
+set -Ux BUN_INSTALL "$HOME/.bun"
+fish_add_path "$HOME/.bun/bin"
 
 # The next line updates PATH for Netlify's Git Credential Helper.
-test -f '/Users/dangvanthanh/Library/Preferences/netlify/helper/path.fish.inc' && source '/Users/dangvanthanh/Library/Preferences/netlify/helper/path.fish.inc'
-# bun
-set -Ux BUN_INSTALL "/Users/dangvanthanh/.bun"
-fish_add_path "/Users/dangvanthanh/.bun/bin"
-
+test -f "$HOME/Library/Preferences/netlify/helper/path.fish.inc" && source "$HOME/Library/Preferences/netlify/helper/path.fish.inc"
