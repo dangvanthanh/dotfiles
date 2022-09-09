7715fe2167bc956c8ec1355f330acd7590815f32
Dang Van Thanh <dangvanthanh@dangthanh.org>
2022-09-09 11:18:00 +0700

    Add multiple github account

---
+++ config/fish/functions/git-datagap.fish
@@ -0,0 +1,4 @@
+function git-datagap
+  git config --global user.name "Dang Van Thanh"
+  git config --global user.email "thanh.dang@datagap.io"
+end
---
+++ config/fish/functions/git-me.fish
@@ -0,0 +1,4 @@
+function git-me
+  git config --global user.name "Dang Van Thanh"
+  git config --global user.email "dangvanthanh@dangthanh.org"
+end
