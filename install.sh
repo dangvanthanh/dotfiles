b1af1d200a1a684e98202084229692ca8f78187a
Dang Van Thanh <dangvanthanh@dangthanh.org>
2023-11-23 18:15:42 -0800

    add kitty terminal

--- install.sh
+++ install.sh
@@ -4,41 +4,52 @@ dotfiles="$HOME/Code/dotfiles"
 
 # Fish
 echo "Setting up Fish"
-ln -svf $dotfiles/config/fish/config.fish $HOME/.config/fish/config.fish
-ln -svf $dotfiles/config/fish/aliases.fish $HOME/.config/fish/aliases.fish
+fish="$dotfiles/config/fish"
+fishHome="$HOME/.config/fish"
+fishFiles=(
+  "config.fish"
+  "aliases.fish"
+)
+
+for file in "${nvimFiles[@]}"; do
+  ln -svf "$nvim/$file" "$nvimHome/$file"
+done
 
 # Neovim
 echo "Setting up Neovim"
 nvim="$dotfiles/config/nvim"
 nvimHome="$HOME/.config/nvim"
-
-files=(
-    "init.lua"
-    "lua/config/autocmds.lua"
-    "lua/config/keymaps.lua"
-    "lua/config/lazy.lua"
-    "lua/config/options.lua"
-    "lua/plugins/alpha.lua"
-    "lua/plugins/copilot.lua"
-    "lua/plugins/disabled.lua"
-    "lua/plugins/gitsigns.lua"
-    "lua/plugins/gruvbox.lua"
-    "lua/plugins/lsp.lua"
-    "lua/plugins/mason.lua"
-    "lua/plugins/neo-tree.lua"
-    "lua/plugins/nvim-cmp.lua"
-    "lua/plugins/plugins.lua"
-    "lua/plugins/telescope.lua"
-    "lua/plugins/treesitter.lua"
+nvimFiles=(
+  "init.lua"
+  "lua/config/autocmds.lua"
+  "lua/config/keymaps.lua"
+  "lua/config/lazy.lua"
+  "lua/config/options.lua"
+  "lua/plugins/alpha.lua"
+  "lua/plugins/copilot.lua"
+  "lua/plugins/disabled.lua"
+  "lua/plugins/gitsigns.lua"
+  "lua/plugins/gruvbox.lua"
+  "lua/plugins/lsp.lua"
+  "lua/plugins/mason.lua"
+  "lua/plugins/neo-tree.lua"
+  "lua/plugins/nvim-cmp.lua"
+  "lua/plugins/plugins.lua"
+  "lua/plugins/telescope.lua"
+  "lua/plugins/treesitter.lua"
 )
 
-for file in "${files[@]}"; do
-    ln -svf "$nvim/$file" "$nvimHome/$file"
+for file in "${nvimFiles[@]}"; do
+  ln -svf "$nvim/$file" "$nvimHome/$file"
 done
 
 # Alacritty
 echo "Setting up Alacritty"
 ln -svf $dotfiles/config/alacritty/alacritty.yml $HOME/.config/alacritty/alacritty.yml
+
+# Kitty
+echo "Setting up Kitty"
+ln -svf $dotfiles/config/kitty/kitty.conf $HOME/.config/kitty/kitty.conf
 
 # Tmux
 echo "Setting up Tmux"
