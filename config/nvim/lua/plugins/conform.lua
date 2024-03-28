f0d95e8e542ca5f2fb1fb19ca478c810397cf74c
Dang Van Thanh <dangvanthanh@dangthanh.org>
2024-03-28 19:28:48 +0700

    feat: update neovim format with biome

--- config/nvim/lua/plugins/conform.lua
+++ config/nvim/lua/plugins/conform.lua
@@ -1,49 +1,48 @@
 return {
   {
     "stevearc/conform.nvim",
-    dependencies = { "mason.nvim" },
-    lazy = true,
+    event = { "BufWritePre" },
     cmd = "ConformInfo",
     keys = {
       {
         "<leader>cF",
         function()
-          require("conform").format({ formatters = { "injected" }, timeout_ms = 3000 })
+          require("conform").format({ formatters = { "injected" }, timeout_ms = 1000 })
         end,
         mode = { "n", "v" },
         desc = "Format Injected Langs",
       },
     },
-    opts = function()
-      local opts = {
-        format = {
-          timeout_ms = 1000,
-          async = false, -- not recommended to change
-          quiet = false, -- not recommended to change
-          lsp_fallback = true, -- not recommended to change
-        },
-        formatters_by_ft = {
-          javascript = { "biome" },
-          typescript = { "biome" },
-          javascriptreact = { "biome" },
-          typescriptreact = { "biome" },
-          svelte = { "biome" },
-          css = { "stylelint" },
-          html = { "djlint" },
-          json = { "biome" },
-          yaml = { "yamlfix" },
-          markdown = { "deno_fmt" },
-          graphql = { "prettier" },
-          python = { "isort", "black" },
-          lua = { "stylua" },
-          fish = { "fish_indent" },
-          sh = { "shfmt" },
-        },
-        formatters = {
-          injected = { options = { ignore_errors = true } },
-        },
-      }
-      return opts
-    end,
+    opts = {
+      format = {
+        timeout_ms = 1000,
+        async = false, -- not recommended to change
+        quiet = false, -- not recommended to change
+        lsp_fallback = true, -- not recommended to change
+      },
+      formatters_by_ft = {
+        ["javascript"] = { "biome" },
+        ["typescript"] = { "biome" },
+        ["javascriptreact"] = { "biome" },
+        ["typescriptreact"] = { "biome" },
+        ["json"] = { "biome" },
+        ["jsonc"] = { "biome" },
+        ["html"] = { "biome" },
+        ["svelte"] = { "biome" },
+        ["astro"] = { "biome" },
+        ["scss"] = { "biome" },
+        ["css"] = { "biome" },
+        ["yaml"] = { "biome" },
+        ["markdown"] = { "biome" },
+        ["markdown.mdx"] = { "biome" },
+        ["graphql"] = { "biome" },
+        ["lua"] = { "stylua" },
+        ["fish"] = { "fish_indent" },
+        ["sh"] = { "shfmt" },
+      },
+      formatters = {
+        injected = { options = { ignore_errors = true } },
+      },
+    },
   },
 }
