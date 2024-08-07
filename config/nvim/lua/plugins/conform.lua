42d114a36fa03c3919d97c24092a7a58b6d17c21
Dang Van Thanh <dangvanthanh@dangthanh.org>
2024-08-07 12:19:11 +0700

    chore: disabled markdown.nvim and update format markdown

--- config/nvim/lua/plugins/conform.lua
+++ config/nvim/lua/plugins/conform.lua
@@ -32,8 +32,8 @@ return {
         ["scss"] = { "biome" },
         ["css"] = { "biome" },
         ["yaml"] = { "biome" },
-        ["markdown"] = { "biome" },
-        ["markdown.mdx"] = { "biome" },
+        ["markdown"] = { "biome", "markdownlint-cli2", "markdown-toc" },
+        ["markdown.mdx"] = { "biome", "markdownlint-cli2", "markdown-toc" },
         ["graphql"] = { "biome" },
         ["lua"] = { "stylua" },
         ["fish"] = { "fish_indent" },
@@ -41,6 +41,23 @@ return {
       },
       formatters = {
         injected = { options = { ignore_errors = true } },
+        ["markdown-toc"] = {
+          condition = function(_, ctx)
+            for _, line in ipairs(vim.api.nvim_buf_get_lines(ctx.buf, 0, -1, false)) do
+              if line:find("<!%-%- toc %-%->") then
+                return true
+              end
+            end
+          end,
+        },
+        ["markdownlint-cli2"] = {
+          condition = function(_, ctx)
+            local diag = vim.tbl_filter(function(d)
+              return d.source == "markdownlint"
+            end, vim.diagnostic.get(ctx.buf))
+            return #diag > 0
+          end,
+        },
       },
     },
   },
--- config/nvim/lua/plugins/disabled.lua
+++ config/nvim/lua/plugins/disabled.lua
@@ -3,5 +3,6 @@ return {
   { "folke/tokyonight.nvim", enabled = false },
   { "catppuccin", enabled = false },
   { "folke/flash.nvim", enabled = false },
-  { "habamax", enabled = false }
+  { "habamax", enabled = false },
+  { "MeanderingProgrammer/markdown.nvim", enabled = false },
 }
