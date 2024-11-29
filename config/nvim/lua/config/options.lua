2234c579514823d698732768bc1d002ca918dd51
Dang Van Thanh <dangvanthanh@dangthanh.org>
2024-11-29 11:11:36 +0700

    chore: modify options for neovim

--- config/nvim/lua/config/options.lua
+++ config/nvim/lua/config/options.lua
@@ -3,12 +3,19 @@
 -- Add any additional options here
 -- Highlight YAML front matter
 vim.opt.guifont = "VictorMono NF"
-vim.api.nvim_set_var("vim_markdown_frontmatter ", 1)
 vim.opt.relativenumber = false
 vim.opt.winbar = "%=%m %f"
+vim.opt.conceallevel = 0
+vim.opt.wrap = true
+vim.opt.linebreak = true
+vim.opt.autoindent = true
+
 vim.g.lazygit_config = false
 vim.g.loaded_python3_provider = 0
 vim.g.loaded_ruby_provider = 0
 vim.g.loaded_node_provider = 0
 vim.g.loaded_perl_provider = 0
 vim.g.lazyvim_python_lsp = "basedpyright"
+vim.g.lazyvim_no_inlay_hints = true
+
+vim.api.nvim_set_var("vim_markdown_frontmatter ", 1)
