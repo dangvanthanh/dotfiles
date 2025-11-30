5cf23efc0602707abf8d0ea4b61263208fbf2057
Dang Van Thanh <dangvanthanh@dangthanh.org>
2025-11-29 21:52:44 -0800

    chore: add pnpm for fish

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -40,3 +40,10 @@ starship init fish | source
 
 # Zoxide
 zoxide init fish | source
+
+# pnpm
+set -gx PNPM_HOME "$HOME/Library/pnpm"
+if not string match -q -- $PNPM_HOME $PATH
+    set -gx PATH "$PNPM_HOME" $PATH
+end
+# pnpm end
