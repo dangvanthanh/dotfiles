d7692bdef6e30150d7ba58d6ca9f643737393b0a
Dang Van Thanh <dangvanthanh@dangthanh.org>
2026-06-03 21:07:49 -0700

    chore: use rust and wasmer in mise

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -7,14 +7,6 @@ set TERM xterm-256color
 set EDITOR nvim
 bind \ct kill_word
 
-# Path
-export PATH="$HOME/.local/bin:$PATH"
-export PATH="$HOME/.cargo/bin:$PATH"
-export PATH="/usr/local/share/dotnet:$PATH"
-export WASMER_DIR="$HOME/.wasmer"
-
-[ -s "$WASMER_DIR/wasmer.sh" ] && source "$WASMER_DIR/wasmer.sh"
-
 # Starship
 starship init fish | source
 
